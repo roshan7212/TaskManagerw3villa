b5e8b7b59ba9d40e88c6e8ee11417035f8a75134
roshan7212 <rairoshan792@gmail.com>
2025-01-24 14:03:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [user, setUser] = useState({});
+  const [taskTitle, setTaskTitle] = useState("Tasks");
+
+  // This effect runs only once when the component mounts
+  useEffect(() => {
+    const handleGetUser = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/user/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(data.user);
+      } catch (error) {
+        console.log("USER IS NOT AUTHENTICATED!");
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    handleGetUser();
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+  return (
+    <>
+      <Router>
+        <Navbar
+          setTasks={setTasks}
+          tasks={tasks}
+          setIsAuthenticated={setIsAuthenticated}
+          isAuthenticated={isAuthenticated}
+          setTaskTitle={setTaskTitle}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                isAuthenticated={isAuthenticated}
+                tasks={tasks}
+                setTasks={setTasks}
+                taskTitle={taskTitle}
+              />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <Register
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Login
+                isAuthenticated={isAuthenticated}
+                setIsAuthenticated={setIsAuthenticated}
+              />
+            }
+          />
+          <Route
+            path="/profile"
+            element={<Profile user={user} isAuthenticated={isAuthenticated} />}
+          />
+        </Routes>
+        <Toaster />
+      </Router>
+    </>
+  );
+};
+
+export default App;
